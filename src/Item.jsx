00c76a361cb3c69048541e9a3639f9a012d3ba19
biltuonqanet <115626436+biltuonqanet@users.noreply.github.com>
2023-03-09 09:14:56 +0000

    Pending changes exported from your codespace

---
+++ src/Item.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+
+const Item = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 header">
+                <h1>welcome to react js live</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Item;
